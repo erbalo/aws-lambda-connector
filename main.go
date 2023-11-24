34f1de31920cd1c3e1e92be850dfc6c7b5f4ab0c
erbalo <ebarreral.isc@gmail.com>
2023-11-23 22:41:52 -0600

    Add code coverage for the project

--- main.go
+++ main.go
@@ -10,9 +10,15 @@ import (
 )
 
 func main() {
-	conf, exit := handleArgs()
-	if conf == nil {
-		os.Exit(exit)
+	conf, err := parser.Parse(os.Args)
+	if err != nil {
+		os.Stderr.WriteString("Error parsing arguments: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+
+	if conf.ShowHelp {
+		parser.ShowHelp()
+		os.Exit(0)
 	}
 
 	dialer := rpc.NewDialer()
@@ -23,21 +29,6 @@ func main() {
 	}
 
 	println(res)
-}
-
-func handleArgs() (*parser.Configuration, int) {
-	conf, err := parser.Parse(os.Args)
-	if err != nil {
-		os.Stderr.WriteString("Error parsing arguments: " + err.Error() + "\n")
-		return nil, 1
-	}
-
-	if conf.ShowHelp {
-		parser.ShowHelp()
-		return nil, 0
-	}
-
-	return conf, 0
 }
 
 func run(conf *parser.Configuration, dialer rpc.Dialer) (string, error) {
